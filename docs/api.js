09074468f870c5ffc47dbb1e75bef57cb7a496ca
Pablo José Cabrera Muñoz <pablo.cabrera.munoz@gmail.com>
2017-08-09 15:53:08 -0300

    Updated docs

--- docs/api.js
+++ docs/api.js
@@ -1,6 +1,19 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
-    "classes": [],
+    "classes": [
+        "chess.Bishop",
+        "chess.Board",
+        "chess.Chess",
+        "chess.King",
+        "chess.Knight",
+        "chess.Move",
+        "chess.Pawn",
+        "chess.Piece",
+        "chess.Place",
+        "chess.Queen",
+        "chess.Rook",
+        "chess.Util"
+    ],
     "modules": [],
     "allModules": []
 } };
